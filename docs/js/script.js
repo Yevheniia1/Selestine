118b3e1ff2442fff48e1b60d821d51620cfa2e47
Yevheniia <y.potiichuk@gmail.com>
2020-01-27 17:29:47 +0200

    Добавлена анимация появления на странице секций и постов

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,53 +1,110 @@
+const header = document.querySelector('main-header'),
+      logo = document.querySelector('.logo>p'),
+      navigationLinks = document.querySelectorAll('.navigation>.list>li'),
+      headerSlider = document.querySelector('.slider--header'),
+      latestSection = document.querySelector('.latest'),
+      latestPosts = document.querySelectorAll('.latest__list>li'),
+      videoSection = document.querySelector('.video'),
+      videoPosts = document.querySelectorAll('.video__wrapper>.blog-post'),
+      featuredSection = document.querySelector('.featured'),
+      featuredPost = document.querySelectorAll('.featured>.blog-post'),
+      popularSection = document.querySelector('.popular'),
+      popularPosts = document.querySelectorAll('.popular__list>li');
 
-let animation_items = new Map();
-
-animation_items.set('opacity', Array.from(document.querySelectorAll('.move-opacity')))
-               .set('scale', Array.from(document.querySelectorAll('.move-scale')))
-               .set('top', Array.from(document.querySelectorAll('.move-top')))
-               .set('right', Array.from(document.querySelectorAll('.move-right')))
-               .set('left', Array.from(document.querySelectorAll('.move-left')))
-               .set('bottom', Array.from(document.querySelectorAll('.move-bottom')));
-
-let animation_arr = [];
-
-(function() {
-  for(let item of animation_items.values()) {
-    animation_arr = animation_arr.concat(item);
-  }
-})()
-
-
-
-function isVisible(elem) {
-  if(elem.className.includes('animation')) {
-    return
-  }
-  let coords = elem.getBoundingClientRect();
-  let windowHeight = document.documentElement.clientHeight;
-  let topVisible = coords.top > 0 && coords.top < windowHeight;
-  return topVisible;
+function scaling(elements) {
+  return  gsap.from(elements, {
+    scale: 0,
+    opacity: 0,
+    ease: "power2.out",
+    duration: 1,
+  });
 }
 
-function showVisible() {
-
-  for (let item of animation_arr) {
-
-  	if (isVisible(item)){
-         animatioin(item);
-  	  }
-  }
+function moveLeft(elements) {
+  return gsap.from(elements, {
+    x: "500",
+    duration: 1,
+    ease: "power1.out",
+    opacity: 0,
+    stagger: .1
+  })
 }
 
-function animatioin(item) {
-  for(let [key, value] of animation_items.entries()) {
-      if(value.includes(item)) {
-        item.classList.add(`animation-${key}`)
-        let index = value.indexOf(item);
-        value.splice(index, 1);
-        return
-      }  
-    }
+function moveRight(elements) {
+  return gsap.from(elements, {
+    x: "-500",
+    duration: 1,
+    ease: "power1.out",
+    opacity: 0,
+    stagger: .1
+  })
 }
 
-showVisible();
-window.onscroll = showVisible;
+function moveUp(elements) {
+  return gsap.from(elements, {
+    y: "500",
+    duration: 1,
+    ease: "power1.out",
+    opacity: 0,
+    stagger: .1
+  })
+}
+
+let controller = new ScrollMagic.Controller();
+
+let headerAnimation = gsap.timeline()
+.add(moveLeft(navigationLinks))
+.add(scaling(headerSlider));
+
+let headerScene = new ScrollMagic.Scene({
+  triggerElement: header,
+  triggerHook: 0,
+  reverse: false,
+})
+  .setTween(headerAnimation)
+  .addTo(controller);
+
+let latestAnimation = gsap.timeline()
+.add(moveUp(latestPosts));
+
+let latestsScene = new ScrollMagic.Scene({
+  triggerElement: latestSection,
+  triggerHook: .8,
+  reverse: false,
+})
+  .setTween(latestAnimation)
+  .addTo(controller);
+
+let videoAnimation = gsap.timeline()
+.add(moveLeft(videoPosts));
+
+let videoScene = new ScrollMagic.Scene({
+  triggerElement: videoSection,
+  triggerHook: .8,
+  reverse: false,
+})
+  .setTween(videoAnimation)
+  .addTo(controller);
+
+let featuredAnimation = gsap.timeline()
+  .add(scaling(featuredPost));
+
+let featuredScene = new ScrollMagic.Scene({
+  triggerElement: featuredSection,
+  triggerHook: .8,
+  reverse: false,
+})
+  .setTween(featuredAnimation)
+  .addTo(controller);
+
+let popularAnimation = gsap.timeline()
+  .add(moveUp(popularPosts));
+
+let popularScene = new ScrollMagic.Scene({
+  triggerElement: popularSection,
+  triggerHook: .8,
+  reverse: false,
+})
+  .setTween(popularAnimation)
+  .addTo(controller);
+  
